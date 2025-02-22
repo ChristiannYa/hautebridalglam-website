8191224394fc517ea451d66fc269da62d8bca591
ChristiannYa <christian.ylopez02@gmail.com>
2025-02-21 22:06:03 -0600

    moved landiong video location

--- src/sections/Home.jsx
+++ src/sections/Home.jsx
@@ -2,17 +2,21 @@ function Home() {
   return (
     <section className="fixed screen overflow-hidden z-0">
       <video autoPlay loop muted className="background-video">
-        <source src="../../src/assets/landing.mp4" type="video/mp4" />
+        <source src="/landing.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
       <div className="text-white text-center absolute-middle">
         <h1 className="font-outfit text-lg">Haute Bridal Glam by A.J.</h1>
-        <h1 className="font-italiana text-5xl uppercase mb-4">Bridal Artistry</h1>
-        <hr className="mb-4"/>
-        <p className="font-outfit font-light text-xl">Enhancing Your Natural Beauty on Your Special Day</p>
+        <h1 className="font-italiana text-5xl uppercase mb-4">
+          Bridal Artistry
+        </h1>
+        <hr className="mb-4" />
+        <p className="font-outfit font-light text-xl">
+          Enhancing Your Natural Beauty on Your Special Day
+        </p>
       </div>
     </section>
-  )
+  );
 }
 
 export default Home
